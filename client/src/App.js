b253710e6cdbf3253e73b8c57216fdc415783779
Kainat <kainat.elahi95@gmailcom>
2022-03-17 13:54:24 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<BrowserRouter>
+				<h1>Our blog</h1>
+			</BrowserRouter>
+		</>
+	);
+}
+
+export default App;
---
+++ sanity/README.md
@@ -0,0 +1,5 @@
+1.	Laste: “ npm install” – client mappe ( react)
+- terminal 
+2.	laste:  “npm install” – sanity mappe
+-terminal
+3.	ny terminal node I sanity mappa - «sanity start»
